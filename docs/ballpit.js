6440176f948e3c122a0a20367e73ab2a64779c56
Stripes <88601301+stripesfensky@users.noreply.github.com>
2026-03-30 22:35:48 -0400

    Update ballpit.js

--- docs/ballpit.js
+++ docs/ballpit.js
@@ -1,29 +1,81 @@
 window.addEventListener("load", () => {
-  var icon = document.querySelector("#header-darkmode-icon");
-  icon.addEventListener("click", () => {
-    darkmode(icon);
+  var theme = localStorage.getItem("theme") || "light";
+  var html = document.querySelector("html");
+  var body = document.querySelector("body");
+  var header = document.querySelector("header");
+  var wordmark = document.querySelector("#header-logo-wordmark");
+  var darkmode_icon = document.querySelector("#header-darkmode-icon");
+  var brandmark = document.querySelector("#header-logo-brandmark");
+  var brandmark_svg = brandmark.contentDocument;
+  var brandmark_svg_pit = brandmark_svg.querySelector("#pit");
+
+  if (theme == "dark") {
+    darkmode(body, header, wordmark, darkmode_icon, brandmark_svg_pit);
+  }
+  else {
+    lightmode(body, header, wordmark, darkmode_icon, brandmark_svg_pit);
+  }
+
+  brandmark_colors(brandmark_svg);
+  html.style.visibility = "visible";
+  
+  darkmode_icon.addEventListener("click", () => {
+    theme = localStorage.getItem("theme") || "light";
+    darkmode_toggle(theme, body, header, wordmark, darkmode_icon, brandmark_svg_pit);
   });
 });
 
-function darkmode(icon){
-  var header = document.querySelector("header");
-  var brandmark = document.querySelector("#header-logo-brandmark");
-  var brandmark_bm_black = brandmark.querySelector("#bm_black");
-  var wordmark = document.querySelector("#header-logo-wordmark");
+function brandmark_colors(brandmark_svg){
+  var brandmark_svg_ball1 = brandmark_svg.querySelector("#ball1");
+  var brandmark_svg_ball2 = brandmark_svg.querySelector("#ball2");
+  var brandmark_svg_ball3 = brandmark_svg.querySelector("#ball3");
+  var styles = getComputedStyle(document.documentElement);
 
-  if (icon.className == "fa-solid fa-moon"){
-    header.style.backgroundColor = "#101010";
-    brandmark_bm_black.setAttribute("fill", "white");
-    wordmark.style.color = "#FFFFFF";
-    icon.className = "fa-regular fa-sun";
-    icon.style.color = "#FFFFFF";
+  var colors = [
+    styles.getPropertyValue("--red"),
+    styles.getPropertyValue("--orange"),
+    styles.getPropertyValue("--green"),
+    styles.getPropertyValue("--purple"),
+  ]
+  
+  /* https://stackoverflow.com/a/46545530 */
+  var brandmark_colors = colors
+    .map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value);
+
+  brandmark_svg_ball1.setAttribute("fill", brandmark_colors[0]);
+  brandmark_svg_ball2.setAttribute("fill", brandmark_colors[1]);
+  brandmark_svg_ball3.setAttribute("fill", brandmark_colors[2]);
+}
+
+function darkmode_toggle(theme, body, header, wordmark, darkmode_icon, brandmark_svg_pit){
+  if (theme === "light") {
+    console.log("Light -> dark");
+    localStorage.setItem("theme", "dark");
+    darkmode(body, header, wordmark, darkmode_icon, brandmark_svg_pit);
   }
-
   else {
-    header.style.backgroundColor = "#FFFFFF"
-    bm_black.setAttribute("fill", "black");
-    wordmark.style.color = "#000000";
-    icon.className = "fa-solid fa-moon";
-    icon.style.color = "#000000";
+    console.log("Dark -> light");
+    localStorage.setItem("theme", "light");
+    lightmode(body, header, wordmark, darkmode_icon, brandmark_svg_pit);
   }
+}
+
+function darkmode(body, header, wordmark, darkmode_icon, brandmark_svg_pit){
+  body.style.backgroundColor = "#222222";
+  header.style.backgroundColor = "#101010";
+  wordmark.style.color = "#FFFFFF";
+  darkmode_icon.className = "fa-solid fa-sun";
+  darkmode_icon.style.color = "#FFFFFF";
+  brandmark_svg_pit.setAttribute("fill", "#FFFFFF");
+}
+
+function lightmode(body, header, wordmark, darkmode_icon, brandmark_svg_pit){
+  body.style.backgroundColor = "#FFFFFF";
+  header.style.backgroundColor = "#FAFAFA";
+  wordmark.style.color = "#000000";
+  darkmode_icon.className = "fa-regular fa-moon";
+  darkmode_icon.style.color = "#000000";
+  brandmark_svg_pit.setAttribute("fill", "#000000");
 }
