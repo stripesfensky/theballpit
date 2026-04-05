e90db857a598119741abc471594a474df39d985f
Stripes <88601301+stripesfensky@users.noreply.github.com>
2026-04-05 18:25:45 -0400

    Update .gitignore

--- viewer/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.apps import apps
-
-for model in apps.get_app_config('viewer').get_models():
-  try:
-    admin.site.register(model)
-  except admin.sites.AlreadyRegistered:
-    pass
