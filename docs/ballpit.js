3b461aef07f5de559e203519161a7ea8cd2d7a58
Stripes <88601301+stripesfensky@users.noreply.github.com>
2026-04-05 15:10:06 -0400

    Update ballpit.js

--- docs/ballpit.js
+++ docs/ballpit.js
@@ -5,7 +5,38 @@ window.addEventListener("load", () => {
   brandmarkColors();
 
   html.style.visibility = "visible";
+
+  var header_search = document.querySelector("#header-search");
+  header_search.addEventListener("click", () => {
+    var nav = document.querySelector("nav");
+    var nav_search = document.querySelector("#nav-search");
+    var nav_settings = document.querySelector("#nav-settings");
+    showNavItem(nav, nav_search, nav_settings);
+  });
+
+  var header_settings = document.querySelector("#header-settings");
+  header_settings.addEventListener("click", () => {
+    var nav = document.querySelector("nav");
+    var nav_search = document.querySelector("#nav-search");
+    var nav_settings = document.querySelector("#nav-settings");
+    showNavItem(nav, nav_settings, nav_search);
+  });
 });
+
+function showNavItem(nav, show, hide) {
+  if (nav.classList.contains("nav-section-hidden")) {
+    if (!hide.classList.contains("nav-hidden")) {
+      hide.classList.add("nav-hidden");
+    }
+    if (show.classList.contains("nav-hidden")) {
+      show.classList.remove("nav-hidden");
+    }
+    nav.classList.remove("nav-section-hidden");
+  }
+  else {
+    nav.classList.add("nav-section-hidden");
+  }
+}
 
 function generateColors() {
   var root = document.documentElement;
